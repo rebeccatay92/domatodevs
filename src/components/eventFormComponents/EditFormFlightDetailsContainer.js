f38c469f275502a8fbb6f7e17dad287fc78435f7
Rebecca Tay <rebeccatay92@gmail.com>
2018-01-21 15:15:13 +0800

    editFlightForm bottom left flight details. header, next,previous page arrows. need to render flights in each page

--- src/components/eventFormComponents/EditFormFlightDetailsContainer.js
+++ src/components/eventFormComponents/EditFormFlightDetailsContainer.js
@@ -3,19 +3,74 @@ import React, { Component } from 'react'
 class EditFormFlightDetailsContainer extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      isReturn: false,
+      totalPages: 1,
+      currentPage: 1,
+      page1: [],
+      page2: []
+    }
+  }
+
+  nextPage () {
+    this.setState({currentPage: 2})
+  }
+  previousPage () {
+    this.setState({currentPage: 1})
   }
 
   componentWillReceiveProps (nextProps) {
     if (this.props.flightInstances !== nextProps.flightInstances) {
       console.log('NEXT PROPS', nextProps)
+      var isReturn = nextProps.returnTrip
+      var flightInstances = nextProps.flightInstances
+      if (isReturn) {
+        if (flightInstances.length === 2) {
+          var page1 = flightInstances[0]
+          var page2 = flightInstances[1]
+        } else if (flightInstances.length === 4) {
+          page1 = flightInstances.slice(0, 2)
+          page2 = flightInstances.slice(2)
+        }
+        this.setState({totalPages: 2, isReturn: true, page1: page1, page2: page2})
+      } else {
+        // one way
+        if (flightInstances.length === 4) {
+          page1 = flightInstances.slice(0, 2)
+          page2 = flightInstances.slice(2)
+          this.setState({totalPages: 2, isReturn: false, page1: page1, page2: page2})
+        } else {
+          page1 = flightInstances
+          this.setState({totalPages: 1, isReturn: false, page1: page1, page2: []})
+        }
+      }
     }
   }
 
   render () {
     return (
       <div style={{position: 'relative'}}>
-        <h1 style={{textAlign: 'center', fontSize: '18px', color: 'white', position: 'relative'}}>Testing</h1>
+        {/* FLIGHT INSTANCE PAGE COMPONENT */}
+        <EditFormFlightDetailsPage instances={this.state[`page${this.state.currentPage}`]} />
+
+        {/* DEPART/RETURN HEADER */}
+        {this.state.currentPage === 1 &&
+        <p style={{textAlign: 'center', fontSize: '18px', color: 'white', position: 'relative'}}>Departing Flights</p>
+        }
+        {this.state.isReturn && this.state.currentPage === 2 &&
+        <p style={{textAlign: 'center', fontSize: '18px', color: 'white', position: 'relative'}}>Returning Flights</p>
+        }
+        {!this.state.isReturn && this.state.currentPage === 2 &&
+        <p style={{textAlign: 'center', fontSize: '18px', color: 'white', position: 'relative'}}>Departing Flights (pg 2)</p>
+        }
+
+        {/* NEXT PAGE / PREVIOUS PAGE ARROWS */}
+        {this.state.totalPages === 2 && this.state.currentPage === 1 &&
+        <i onClick={() => this.nextPage()} className='material-icons' style={{cursor: 'pointer', position: 'absolute', top: '172px', right: '10px', fontSize: '50px'}}>chevron_right</i>
+        }
+        {this.state.totalPages === 2 && this.state.currentPage === 2 &&
+        <i onClick={() => this.previousPage()} className='material-icons' style={{cursor: 'pointer', position: 'absolute', top: '172px', left: '10px', fontSize: '50px'}}>chevron_left</i>
+        }
       </div>
     )
   }
