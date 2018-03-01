cd37783f37c25d9bdfa631724eabde539a3ea513
Rebecca Tay <rebeccatay92@gmail.com>
2018-03-01 14:49:52 +0800

    stash before trying to sync days filter between map and sidebar

--- src/reducers/mapPlannerCurrentFocusReducer.js
+++ src/reducers/mapPlannerCurrentFocusReducer.js
@@ -1,7 +1,6 @@
 export const mapPlannerCurrentFocusReducer = (state = {modelId: '', eventType: '', flightInstanceId: '', day: 0, start: null, loadSequence: 0}, action) => {
   switch (action.type) {
     case 'SET_CURRENTLY_FOCUSED_EVENT':
-      // console.log('obj', action.currentlyFocusedEvent)
       return action.currentlyFocusedEvent
     case 'CLEAR_CURRENTLY_FOCUSED_EVENT':
       return {modelId: '', eventType: '', flightInstanceId: '', day: 0, start: null, loadSequence: 0}
