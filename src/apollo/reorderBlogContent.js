56eb77c9cfa7408b528fd21c9992d7d213ed0007
Rebecca Tay <rebeccatay92@gmail.com>
2018-04-03 09:03:42 +0800

    fix apollo for reorderBlogContent

--- src/apollo/reorderBlogContent.js
+++ src/apollo/reorderBlogContent.js
@@ -2,14 +2,10 @@ import { gql } from 'react-apollo'
 
 export const reorderBlogContent = gql`
   mutation reorderBlogContent(
-    $type: String!,
-    $modelId: ID!,
-    $loadSequence: Int!
+    $input: [reorderBlogContentInput]
   ) {
     reorderBlogContent(
-      type: $type,
-      modelId: $modelId,
-      loadSequence: $loadSequence
+      input: $input
     )
   }
 `
