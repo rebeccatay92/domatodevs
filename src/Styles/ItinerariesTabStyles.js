88fcc43c0368f0fc7ce6017f938fbf7e12f4efcf
Rebecca Tay <rebeccatay92@gmail.com>
2018-05-11 15:01:19 +0800

    dashboard itineraries tab. need privacy, redirect logic. placeholder text for hashtags and budget

--- src/Styles/ItinerariesTabStyles.js
+++ src/Styles/ItinerariesTabStyles.js
@@ -1,14 +1,159 @@
 import { coreColors, coreFonts } from './cores'
 
+// NEEDS RADIUM ?  YES. HOVER OVER ITINERARY ROW
 const itinerariesTabContainer = {
   width: '1265px',
-  height: 'calc(100vh - 108px)' // navbar + tabs bar
+  // navbar + tabsbar
+  minHeight: 'calc(100vh - 108px)',
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  justifyContent: 'flex-start',
+  paddingBottom: '90px' // 1 row height
 }
 
 const itineraryContainer = {
-  width: '1265px'
+  width: '1265px',
+  // height: '90px',
+  height: '130px',
+  padding: '20px 0',
+  // margin: '20px 0',
+  display: 'flex',
+  cursor: 'pointer',
+  ':hover': {
+    background: 'rgb(245, 245, 245)'
+  }
 }
 
+/* ----------------------------- */
+
+const timeSinceCreatedContainer = {
+  width: '100px',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const timeSinceCreated = {
+  ...coreFonts.garamondRegular,
+  fontSize: '14px',
+  color: coreColors.blackSolid
+}
+/* ----------------------------- */
+const daysDatesCountriesContainer = {
+  width: '100px',
+  height: '100%',
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRight: `1px solid ${coreColors.black03}`
+}
+
+const daysNumber = {
+  ...coreFonts.garamondRegular,
+  fontSize: '32px',
+  lineHeight: '36px',
+  color: coreColors.blackSolid,
+  padding: 0
+}
+
+const daysCountriesText = {
+  ...coreFonts.garamondRegular,
+  fontSize: '14px',
+  lineHeight: '24px',
+  color: coreColors.blackSolid
+}
+/* ----------------------------- */
+const itineraryDetailsContainer = {
+  width: '765px',
+  height: '100%',
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  justfifyContent: 'flex-start',
+  padding: '0 20px'
+}
+
+const itineraryName = {
+  ...coreFonts.garamondRegular,
+  fontSize: '24px',
+  color: coreColors.blackSolid,
+  margin: 0,
+  lineHeight: '40px'
+}
+
+const itineraryDescription = {
+  ...coreFonts.robotoLight,
+  fontSize: '14px',
+  color: coreColors.blackSolid,
+  margin: 0,
+  lineHeight: '24px',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap'
+}
+
+const itineraryTagsRow = {
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'flex-start',
+  height: '18px'
+}
+
+const itineraryTags = {
+  ...coreFonts.garamondRegular,
+  color: coreColors.greenSolid,
+  fontSize: '14px',
+  lineHeight: '18px'
+}
+
+const itineraryTagsSpacer = {
+  color: coreColors.greenSolid,
+  fontSize: '14px',
+  lineHeight: '18px',
+  margin: '0 5px'
+}
+
+/* ----------------------------- */
+
+const budgetContainer = {
+  width: '200px',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const budgetText = {
+  ...coreFonts.garamondRegular,
+  color: coreColors.blackSolid,
+  fontSize: '24px'
+}
+
+const privacyToggleContainer = {
+  width: '100px',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+/* ----------------------------- */
 export const ItinerariesTabStyles = {
-  itinerariesTabContainer
+  itinerariesTabContainer,
+  itineraryContainer,
+  timeSinceCreatedContainer,
+  timeSinceCreated,
+  daysDatesCountriesContainer,
+  daysNumber,
+  daysCountriesText,
+  itineraryDetailsContainer,
+  itineraryName,
+  itineraryDescription,
+  itineraryTagsRow,
+  itineraryTags,
+  itineraryTagsSpacer,
+  budgetContainer,
+  budgetText,
+  privacyToggleContainer
 }
--- src/components/dashboard/ItinerariesTab.js
+++ src/components/dashboard/ItinerariesTab.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { graphql } from 'react-apollo'
+import Radium from 'radium'
+
 import { itinerariesByUser } from '../../apollo/itinerary'
+import { setStickyTabs } from '../../actions/userDashboardActions'
 
 import { ItinerariesTabStyles as styles } from '../../Styles/ItinerariesTabStyles'
 
@@ -8,17 +12,72 @@ class ItinerariesTab extends Component {
   constructor (props) {
     super(props)
     this.state = {}
+    this.handleScrollBound = (e) => this.handleScroll(e)
   }
+  componentDidMount () {
+    document.addEventListener('scroll', this.handleScrollBound)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('scroll', this.handleScrollBound)
+  }
+
+  handleScroll (e) {
+    const el = document.querySelector('.itinerariesTabComponent')
+    if (!el) return
+    const rect = el.getBoundingClientRect()
+    const distFromTop = rect.top
+    // console.log('component distFromTop', distFromTop)
+    // 108 px is navbar + horizontal tabs
+    if (distFromTop >= 108 && this.props.userDashboard.stickyTabs) {
+      this.props.setStickyTabs(false)
+    }
+  }
+
   render () {
     if (this.props.data.loading) return (<h1>Loading</h1>)
-    console.log('itineraries', this.props.data.itinerariesByUser)
+    // console.log('itineraries', this.props.data.itinerariesByUser)
     let itineraries = this.props.data.itinerariesByUser
     return (
-      <div style={styles.itinerariesTabContainer}>
+      <div className='itinerariesTabComponent' style={styles.itinerariesTabContainer}>
         {itineraries.map((itinerary, i) => {
           return (
-            <div key={i}>
-              {itinerary.name}
+            <div key={i} style={styles.itineraryContainer}>
+              <div style={styles.timeSinceCreatedContainer}>
+                <span style={styles.timeSinceCreated}>2 years ago</span>
+              </div>
+              <div style={styles.daysDatesCountriesContainer}>
+                <div>
+                  <span style={styles.daysNumber}>{itinerary.days} </span>
+                  <span style={styles.daysCountriesText}>Days</span>
+                </div>
+                {itinerary.countries.map((country, i) => {
+                  return <span style={styles.daysCountriesText}>{country.name}</span>
+                })}
+              </div>
+              <div style={styles.itineraryDetailsContainer}>
+                <h1 style={styles.itineraryName}>{itinerary.name}</h1>
+                <h4 style={styles.itineraryDescription}>{itinerary.description}</h4>
+                <div style={styles.itineraryTagsRow}>
+                  {['Adventure', 'Korea', 'Budget', '2018', 'City'].map((hashtag, i) => {
+                    return (
+                      <div key={`itineraryHashtagDiv${i}`}>
+                        {i !== 0 &&
+                          <span style={styles.itineraryTagsSpacer}>&#8226;</span>
+                        }
+                        <span style={styles.itineraryTags}>{hashtag}</span>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+              <div style={styles.budgetContainer}>
+                <span style={styles.budgetText}>SGD 1000 / pax</span>
+              </div>
+              <div style={styles.privacyToggleContainer}>
+                {/* <i className='material-icons'>visibility</i> */}
+                <i className='material-icons'>lock_outline</i>
+              </div>
             </div>
           )
         })}
@@ -27,4 +86,18 @@ class ItinerariesTab extends Component {
   }
 }
 
-export default graphql(itinerariesByUser)(ItinerariesTab)
+const mapStateToProps = (state) => {
+  return {
+    userDashboard: state.userDashboard
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setStickyTabs: (sticky) => {
+      dispatch(setStickyTabs(sticky))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(graphql(itinerariesByUser)(Radium(ItinerariesTab)))
