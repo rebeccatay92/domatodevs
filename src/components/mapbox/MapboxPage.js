1452f5f7ee1ea522c380deb0a75c86fb1a85229c
Rebecca Tay <rebeccatay92@gmail.com>
2018-05-13 15:01:57 +0800

    added zoom control to map. will sync state zoom, center when zoom, pan happens in map

--- src/components/mapbox/MapboxPage.js
+++ src/components/mapbox/MapboxPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 // import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl'
-import ReactMapboxGL from 'react-mapbox-gl'
+import ReactMapboxGL, { ZoomControl } from 'react-mapbox-gl'
 
+// factory
 const Map = ReactMapboxGL({
   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 })
+
+const mapStyle = 'mapbox://styles/mapbox/streets-v10'
 
 class MapboxPage extends Component {
   constructor (props) {
@@ -19,13 +22,42 @@ class MapboxPage extends Component {
     //     zoom: 8
     //   }
     // }
+    this.state = {
+      center: [0, 0], // long/lat in that order to match GeoJSON
+      zoom: [0], // needs to be wrapped in array
+      containerStyle: {
+        height: '872px',
+        width: '1585px' // has to start with larger version. if smaller, changing containerStyle does not fetch more tiles.
+      }
+    }
   }
-  // for 1920px width, map will toggle betwen 1580 and 1260
+  // for 1920px width, map will toggle betwen 1585 and 1265
+  // hook it up for state of event sidebar
   toggleMapSize () {
-    // this.setState({viewport: {
-    //   ...this.state.viewport,
-    //   width: this.state.viewport.width === 1585 ? 1265 : 1585
-    // }})
+    this.setState({
+      containerStyle: {
+        ...this.state.containerStyle,
+        width: this.state.containerStyle.width === '1585px' ? '1265px' : '1585px'
+      }
+    })
+  }
+
+  // synx state with map's final zoom and center
+  // this only handles zoom. what about drag/pan?
+  // onMapZoomEnd (map, evt) {
+  //   console.log('zoom end center', map.getCenter())
+  //   this.setState({
+  //     zoom: [map.getZoom()],
+  //     center: [map.getCenter().lng, map.getCenter().lat]
+  //   }, () => console.log('updated state', this.state))
+  // }
+
+  // more generic. fires for dragging, zooming, anything as long as bounds change
+  onMapMoveEnd (map, evt) {
+    this.setState({
+      zoom: [map.getZoom()],
+      center: [map.getCenter().lng, map.getCenter().lat]
+    }, () => console.log('updated state after move-end', this.state))
   }
 
   render () {
@@ -34,16 +66,8 @@ class MapboxPage extends Component {
         <div style={{width: '335px', height: '872px', border: '1px solid black'}}>
           <button onClick={() => this.toggleMapSize()}>Change map size</button>
         </div>
-        {/* <ReactMapGL {...this.state.viewport} mapStyle={'mapbox://styles/mapbox/streets-v10'} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN} onViewportChange={(viewport) => this.setState({viewport})}>
-          <div style={{position: 'absolute', left: 0}}>
-            <NavigationControl onViewportChange={(viewport) => this.setState({viewport})} showCompass={false} />
-          </div>
-          <Marker latitude={0} longitude={0}>
-            <div style={{borderRadius: '50%', width: '50px', height: '50px', background: 'red'}}>Testing</div>
-          </Marker>
-        </ReactMapGL> */}
-        <Map style='mapbox://styles/mapbox/streets-v10' zoom={[0]} containerStyle={{height: '872px', width: '1265px'}}>
-          
+        <Map style={mapStyle} zoom={this.state.zoom} containerStyle={this.state.containerStyle} onStyleLoad={el => { this.map = el }} onMoveEnd={(map, evt) => this.onMapMoveEnd(map, evt)}>
+          <ZoomControl position='top-left' />
         </Map>
       </div>
     )
@@ -51,3 +75,12 @@ class MapboxPage extends Component {
 }
 
 export default MapboxPage
+
+{/* <ReactMapGL {...this.state.viewport} mapStyle={'mapbox://styles/mapbox/streets-v10'} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN} onViewportChange={(viewport) => this.setState({viewport})}>
+<div style={{position: 'absolute', left: 0}}>
+<NavigationControl onViewportChange={(viewport) => this.setState({viewport})} showCompass={false} />
+</div>
+<Marker latitude={0} longitude={0}>
+<div style={{borderRadius: '50%', width: '50px', height: '50px', background: 'red'}}>Testing</div>
+</Marker>
+</ReactMapGL> */}
