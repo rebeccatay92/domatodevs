d90e95b10dc88a962d7b145353391e88d31bb366
Rebecca Tay <rebeccatay92@gmail.com>
2018-05-21 11:53:31 +0800

    location cell will show spinner, and query only if str is longer than 2 chars. dropdown display needs refinement

--- src/components/planner/EventRowLocationCell.js
+++ src/components/planner/EventRowLocationCell.js
@@ -8,6 +8,8 @@ import { changeActiveField } from '../../actions/planner/activeFieldActions'
 
 import _ from 'lodash'
 
+import { ClipLoader } from 'react-spinners'
+
 class EventRowLocationCell extends Component {
   constructor (props) {
     super(props)
@@ -23,23 +25,44 @@ class EventRowLocationCell extends Component {
       editorState: EditorState.createWithContent(locationContentState),
       queryStr: '',
       showDropdown: false,
+      showSpinner: false,
       predictions: []
     }
 
+    // FOR NOW, PREVENT QUERYING ON EACH CLICK/FOCUS/UNFOCUS BY COMPARING OLD PLAIN TEXT WITH INCOMING PLAIN TEXT.
+    // THIS MEANS THE FIRST CLICK/TAB TO FOCUS WILL NOT CAUSE A DROPDOWN
     this.onChange = (editorState) => {
-      this.setState({editorState: editorState})
-      const contentState = editorState.getCurrentContent()
-      // id, property, value, fromSidebar
-      this.props.updateEvent(this.props.id, 'location', contentState, false)
+      let oldContentState = this.state.editorState.getCurrentContent()
+      let newContentState = editorState.getCurrentContent()
 
-      // ON CHANGE UPDATE QUERY STR
-      const queryStrFromContentState = contentState.getPlainText()
-      // console.log('plain text querystr', queryStrFromContentState)
-      this.setState({
-        queryStr: queryStrFromContentState,
-        showDropdown: true // requires losing focus, or select result to close results dropdown
-      })
-      this.queryGooglePlaces(queryStrFromContentState)
+      let oldText = oldContentState.getPlainText()
+      let newText = newContentState.getPlainText()
+
+      this.setState({editorState: editorState})
+      // id, property, value, fromSidebar
+      this.props.updateEvent(this.props.id, 'location', newContentState, false)
+
+      // ONLY UPDATE QUERY STR IF STR IS DIFFERENT
+      // IF BACKSPACE TO EMPTY STRING, SPINNER IS TRUE, BUT QUERY DOES NOT RUN. HENCE SPINNER WILL NOT SET BACK TO FALSE
+      if (newText !== oldText) {
+        // dont query useless fragments
+        if (newText.length > 2) {
+          this.setState({
+            queryStr: newText,
+            showDropdown: true,
+            showSpinner: true,
+            predictions: []
+          })
+          this.queryGooglePlaces(newText)
+        } else {
+          this.setState({
+            queryStr: newText,
+            showDropdown: false,
+            showSpinner: false,
+            predictions: []
+          })
+        }
+      }
     }
 
     this.focus = (e) => {
@@ -53,7 +76,7 @@ class EventRowLocationCell extends Component {
   }
 
   queryGooglePlaces (queryStr) {
-    if (!queryStr) return
+    if (queryStr.length <= 2) return // dont query for useless fragments. 'eg Si'
     console.log('query google with', queryStr)
 
     let crossOriginUrl = `https://cors-anywhere.herokuapp.com/`
@@ -63,19 +86,20 @@ class EventRowLocationCell extends Component {
     //   'Allow-Access-Control-Origin': '*'
     // })
 
-    // fetch(googlePlacesEndpoint)
-    //   .then(response => {
-    //     return response.json()
-    //   })
-    //   .then(json => {
-    //     console.log('places response', json.predictions)
-    //     this.setState({
-    //       predictions: json.predictions
-    //     })
-    //   })
-    //   .catch(err => {
-    //     console.log('err', err)
-    //   })
+    fetch(googlePlacesEndpoint)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        console.log('places response', json.predictions)
+        this.setState({
+          predictions: json.predictions,
+          showSpinner: false
+        })
+      })
+      .catch(err => {
+        console.log('err', err)
+      })
   }
 
   componentWillReceiveProps (nextProps) {
@@ -105,11 +129,22 @@ class EventRowLocationCell extends Component {
   render () {
     const isActive = this.props.activeEventId === this.props.id && this.props.activeField === 'location'
     return (
-      <div className='planner-table-cell' onClick={this.focus} style={{cursor: 'text', minHeight: '83px', display: 'flex', alignItems: 'center', wordBreak: 'break-word', outline: isActive ? '1px solid #ed685a' : 'none', color: isActive ? '#ed685a' : 'rgba(60, 58, 68, 1)'}}>
+      <div className='planner-table-cell' onClick={this.focus} style={{position: 'relative', cursor: 'text', minHeight: '83px', display: 'flex', alignItems: 'center', wordBreak: 'break-word', outline: isActive ? '1px solid #ed685a' : 'none', color: isActive ? '#ed685a' : 'rgba(60, 58, 68, 1)'}}>
         <Editor editorState={this.state.editorState} onChange={this.onChange} ref={element => { this.editor = element }} onFocus={() => {
           this.props.changeActiveField('location')
           this.props.updateActiveEvent(this.props.id)
-        }} onBlur={() => this.setState({focusClicked: false})} />
+        }} onBlur={() => this.setState({focusClicked: false, showDropdown: false})} />
+        {this.state.showDropdown &&
+          <div style={{position: 'absolute', top: '83px', left: 0, background: 'white', width: '100%', border: '1px solid red', zIndex: '2', minHeight: '35px'}}>
+            <ClipLoader color={'#000000'} size={35} loading={this.state.showSpinner} />
+            {!this.state.showSpinner && this.state.predictions.length === 0 &&
+              <h6 style={{background: 'white', margin: 0, cursor: 'pointer', minHeight: '35px', fontFamily: 'Roboto, san-serif', fontWeight: '300', fontSize: '16px', lineHeight: '24px', padding: '8px'}}>No results found</h6>
+            }
+            {!this.state.showSpinner && this.state.predictions.map((prediction, i) => {
+              return <h6 key={i} style={{background: 'white', margin: 0, cursor: 'pointer', minHeight: '35px', fontFamily: 'Roboto, san-serif', fontWeight: '300', fontSize: '16px', lineHeight: '24px', padding: '8px'}}>{prediction.structured_formatting.main_text}</h6>
+            })}
+          </div>
+        }
       </div>
     )
   }
