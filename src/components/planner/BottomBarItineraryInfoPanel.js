d05db3fe7a51c98f73a2af68860490695f917db3
Rebecca Tay <rebeccatay92@gmail.com>
2018-06-07 12:13:41 +0800

    change itinerary info panel input field styling

--- src/components/planner/BottomBarItineraryInfoPanel.js
+++ src/components/planner/BottomBarItineraryInfoPanel.js
@@ -10,16 +10,16 @@ import { connect } from 'react-redux'
 import { updateItinerary } from '../../actions/planner/itineraryActions'
 
 class BottomBarItineraryInfoPanel extends Component {
-  constructor (props) {
-    super(props)
-    // this.state = {
-    //   name: props.itinerary.name || '',
-    //   description: props.itinerary.description || '',
-    //   days: props.itinerary.days,
-    //   startDate: props.itinerary.startDate, // unix secs
-    //   // countries: props.itinerary.countries
-    // }
-  }
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {
+  //     name: props.itinerary.name || '',
+  //     description: props.itinerary.description || '',
+  //     days: props.itinerary.days,
+  //     startDate: props.itinerary.startDate, // unix secs
+  //     // countries: props.itinerary.countries
+  //   }
+  // }
 
   handleClickOutside () {
     this.props.closePanel()
@@ -27,21 +27,18 @@ class BottomBarItineraryInfoPanel extends Component {
 
   handleChange (e, field) {
     // days, name, description
-    this.props.updateItinerary(field, e.target.value)
+    if (field === 'days') {
+      this.props.updateItinerary(field, parseInt(e.target.value))
+    } else {
+      this.props.updateItinerary(field, e.target.value)
+    }
   }
 
   onDateChange (e) {
     if (e) {
       let unix = moment(e._d).unix()
-      // console.log('unix', unix)
-      // this.setState({
-      //   startDate: unix
-      // })
       this.props.updateItinerary('startDate', unix)
     } else {
-      // this.setState({
-      //   startDate: null
-      // })
       this.props.updateItinerary('startDate', null)
     }
   }
@@ -53,7 +50,7 @@ class BottomBarItineraryInfoPanel extends Component {
         <div style={{display: 'flex', flexDirection: 'column', width: '100%', minHeight: '340px', boxSizing: 'border-box'}}>
           <h6 style={labelTextStyle}>Title</h6>
           <div style={{width: '100%', minHeight: '35px'}}>
-            <input type='text' value={this.props.itineraryDetails.name} onChange={e => this.handleChange(e, 'name')} />
+            <input type='text' value={this.props.itineraryDetails.name} onChange={e => this.handleChange(e, 'name')} style={{background: 'rgb(245, 245, 245)', border: 'none', height: '35px', outline: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 300, fontSize: '16px', color: 'rgb(60, 58, 68)'}} />
           </div>
           <hr style={{margin: 0, border: '1px solid rgba(60, 58, 68, 0.1)'}} />
           <h6 style={labelTextStyle}>Countries</h6>
@@ -62,18 +59,18 @@ class BottomBarItineraryInfoPanel extends Component {
               return <button key={i}>{country.name}</button>
             })}
           </div>
-          {/*CALL CREATECOUNTRIESITINERARIES / DELETECOUNTRIESITINERARIES DIRECTLY? */}
+          {/* CALL CREATECOUNTRIESITINERARIES / DELETECOUNTRIESITINERARIES DIRECTLY? */}
           <hr style={{margin: 0, border: '1px solid rgba(60, 58, 68, 0.1)'}} />
           <h6 style={labelTextStyle}>Days</h6>
           <div style={{width: '100%', minHeight: '35px'}}>
-            <input type='number' value={this.props.itineraryDetails.days} onChange={e => this.handleChange(e, 'days')} />
+            <input type='number' value={this.props.itineraryDetails.days} min='1' max='30' onChange={e => this.handleChange(e, 'days')} style={{background: 'rgb(245, 245, 245)', border: 'none', height: '35px', outline: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 300, fontSize: '16px', color: 'rgb(60, 58, 68)'}} />
           </div>
           <h6 style={labelTextStyle}>Start Date</h6>
           <DatePicker customInput={<CustomDateInput />} dateFormat={'ddd, DD MMM YYYY'} selected={this.props.itineraryDetails.startDate ? moment.unix(this.props.itineraryDetails.startDate).utc() : null} onChange={e => this.onDateChange(e)} isClearable />
           <hr style={{margin: 0, border: '1px solid rgba(60, 58, 68, 0.1)'}} />
           <h6 style={labelTextStyle}>Description</h6>
           <div style={{width: '100%', minHeight: '35px'}}>
-            <textarea value={this.props.itineraryDetails.description} onChange={e => this.handleChange(e, 'description')} style={{width: '100%', height: '70px'}} />
+            <textarea value={this.props.itineraryDetails.description} onChange={e => this.handleChange(e, 'description')} style={{width: '100%', height: '70px', background: 'rgb(245, 245, 245)', border: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 300, fontSize: '16px', lineHeight: '24px', color: 'rgb(60, 58, 68)', outline: 'none'}} />
           </div>
         </div>
         <div style={{width: 0, height: 0, borderLeft: '10px solid transparent', borderRight: '10px solid transparent', borderTop: '10px solid rgb(245, 245, 245)', position: 'absolute', left: '160px', zIndex: 2}} />
