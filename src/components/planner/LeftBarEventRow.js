84e0a83cd615bb69ab02aa4321f5bbd82bdaa51d
Rebecca Tay <rebeccatay92@gmail.com>
2018-06-12 12:42:56 +0800

    left bar event row will show end time if it exists

--- src/components/planner/LeftBarEventRow.js
+++ src/components/planner/LeftBarEventRow.js
@@ -40,8 +40,14 @@ class LeftBarEventRow extends Component {
             <i className='material-icons' style={{color: isActiveEvent ? 'red' : 'rgb(67, 132, 150)'}}>not_listed_location</i>
           }
         </div>
-        <div style={{width: '113px', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{width: '113px', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
           <input type='time' value={startTime} style={{fontFamily: 'Roboto, sans-serif', fontWeight: '300', fontSize: '16px', color: 'rgba(60, 58, 68, 1)', outline: 'none', background: 'inherit'}} disabled />
+          {this.props.event.endTime &&
+            <React.Fragment>
+              <div style={{width: '1px', height: '10px', borderRight: '1px solid rgb(60, 58, 68)'}} />
+              <input type='time' value={this.props.event.endTime} style={{fontFamily: 'Roboto, sans-serif', fontWeight: '300', fontSize: '16px', color: 'rgba(60, 58, 68, 1)', outline: 'none', background: 'inherit'}} disabled />
+            </React.Fragment>
+          }
         </div>
         <div style={{width: '226px', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
           <span>{eventType || '---'}</span>
