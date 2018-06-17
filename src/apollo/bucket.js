a8971aaec1e8bb78ba02c3d12e14a15e335ffd37
Rebecca Tay <rebeccatay92@gmail.com>
2018-06-17 12:12:05 +0800

    bucket apollo crud

---
+++ src/apollo/bucket.js
@@ -0,0 +1,124 @@
+import { gql } from 'react-apollo'
+
+// depends on context
+export const getUserBucketList = gql`
+  query getUserBucketList {
+    id
+    UserId
+    LocationId
+    location {
+      id
+      verified
+      name
+      address
+      latitude
+      longitude
+    }
+    notes
+    eventType
+    bucketCategory
+    thumbnailUrl
+    visited
+  }
+`
+
+export const findBucket = gql`
+  query findBucket(id: ID!) {
+    findBucket(id: $id) {
+      id
+      UserId
+      LocationId
+      location {
+        id
+        verified
+        name
+        address
+        latitude
+        longitude
+      }
+      notes
+      eventType
+      bucketCategory
+      thumbnailUrl
+      visited
+    }
+  }
+`
+
+export const createBucket = gql`
+  mutation createBucket(
+    $UserId: ID!,
+    $LocationId: ID!,
+    $bucketCategory: String,
+    $eventType: String,
+    $notes: String,
+    $thumbnailUrl: String
+  ) {
+    createBucket(
+      UserId: $UserId,
+      LocationId: $LocationId,
+      bucketCategory: $bucketCategory,
+      eventType: $eventType,
+      notes: $notes,
+      thumbnailUrl: $thumbnailUrl
+    ) {
+      id
+      UserId
+      LocationId
+      location {
+        id
+        verified
+        name
+        address
+        latitude
+        longitude
+      }
+      eventType
+      bucketCategory
+      notes
+      thumbnailUrl
+    }
+  }
+`
+
+export const updateBucket = gql`
+  mutation updateBucket(
+    $id: ID!,
+    $bucketCategory: String,
+    $eventType: String,
+    $notes: String,
+    $thumbnailUrl: String,
+    $visited: Boolean
+  ) {
+    updateBucket(
+      id: $id,
+      bucketCategory: $bucketCategory,
+      eventType: $eventType,
+      notes: $notes,
+      thumbnailUrl: $thumbnailUrl,
+      visited: $visited
+    ) {
+      id
+      UserId
+      LocationId
+      location {
+        id
+        verified
+        name
+        address
+        latitude
+        longitude
+      }
+      eventType
+      bucketCategory
+      notes
+      thumbnailUrl
+    }
+  }
+`
+
+export const deleteBucket = gql`
+  mutation deleteBucket($id: ID!) {
+    deleteBucket(id: $id)
+  }
+`
